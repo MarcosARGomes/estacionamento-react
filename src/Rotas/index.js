6219d10ef2c3360de89913b027b81faad4c73184
Marcos André Ribeiro Gomes <marcosandrebahia@gmail.com>
2021-06-23 13:00:29 -0300

    repositório com alterações do home e css

--- src/Rotas/index.js
+++ src/Rotas/index.js
@@ -4,8 +4,10 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from '../paginas/Home';
 import Contato from '../paginas/Contato';
 import Login from '../paginas/Login';
-import CadastroEstacionamento from '../paginas/cadastros/estacionamento/index';
-import CadastroUsuario from '../paginas/cadastros/usuarioComum/index';
+import Login2 from '../paginas/login2';
+import CadastroEstacionamento from '../componentes/FormulariosCadastro/Estacionamento/index';
+import VagasEstacionamento from '../paginas/VagasEstacionamento'
+import Cadastro from '../paginas/Cadastro'
 
 import Topo from '../componentes/Topo';
 import Rodape from '../componentes/Rodape';
@@ -20,8 +22,10 @@ const Rotas = () => (
                     <Route exact path='/' component={ Home } />
                     <Route exact path='/contato' component={ Contato } />
                     <Route exact path='/Login' component={ Login } />
+                    <Route exact path='/Cadastro' component={Cadastro}/>
                     <Route exact path='/CadastroEstacionamento' component={ CadastroEstacionamento } />
-                    <Route exact path='/CadastroUsuario' component={ CadastroUsuario } />
+                    <Route exact path='/Vagas' component={ VagasEstacionamento } />
+                    <Route exact path='/Login2' component={Login2}/>
                 </Switch>
             <Rodape />
         </BrowserRouter>
